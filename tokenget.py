963d326d7d29f111b74b47f8aeffb007350a7a27
Kyle Squizzato <kyle.squizzato@docker.com>
2018-08-07 15:42:04 -0400

    Get rid of retry_this() and simplify the whole thing
    Signed-off-by: Kyle Squizzato <kyle.squizzato@docker.com>

--- tokenget.py
+++ tokenget.py
@@ -16,31 +16,6 @@ from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 """
-Perform retrying for error handling.
-Specify componentName which is breaking.
-retrySecs is the number of seconds each retry will increment by.
-doSomething handles a function to call at the > 3 retry mark.
-maxAttempts is an int representing the max number of tries before exiting
-(defaults to 1).
-"""
-def retry_this(componentName, retrySecs, retries, maxAttempts=1,
-               doSomething=None):
-    retry_time = int(retries)*retrySecs
-    if retries > 3:
-        # if doSomething isn't given then assume we don't need to run anything
-        # at retries > 3
-        if not doSomething == None:
-            doSomething
-    if retries > maxAttempts:
-        # Notify user we failed
-        logging.error('Unable to access {0} after {1} connection attempts, exiting'.format(componentName, maxAttempts))
-        sys.exit(1)
-    else:
-        logging.info('Unable to access {0}, attempting to reconnect to {0} in {1} seconds'.format(componentName, retry_time))
-        sleep(retry_time)
-        logging.info("Attempting to reconnect to {0} -- retry {1} of {2}".format(componentName, retries, maxAttempts))
-
-"""
 Get and return an authtoken from a given UCP URL
 """
 def get_token(username, password, url, retries=0):
@@ -53,8 +28,7 @@ def get_token(username, password, url, retries=0):
             verify=False)
     except requests.exceptions.RequestException as e:
         logging.error('Failed to authenticate with UCP {0}: {1}'.format(url, e))
-        retries+=1
-        retry_this('UCP', 10, retries, 3, get_token(username, password, url))
+        sys.exit(1)
     logging.debug('Got response: {}'.format(r.text))
     if "unauthorized" in r.text:
         logging.error('Unable to authenticate with UCP {}, are the admin credentials correct?'.format(url))
